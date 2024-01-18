16a3c94b2310ec57dd0ab0e127d3ac8d010ff417
IBTIISAM <ibtesam-almohsen@hotmail.com>
2024-01-18 17:07:06 +0300

    updated navbar

--- components/modetoggle.tsx
+++ components/modetoggle.tsx
@@ -7,12 +7,14 @@ import { Button } from "./ui/button";
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
   return (
-    
-<Button variant="ghost" size="icon" aria-label="Toggle Theme" className="border-l ml-6 mr-6" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
-    <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-    <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-    <span className="sr-only">Toggle Theme</span>
-</Button>
+    <div className="border-l pl-3">
+        <Button variant="ghost" size="icon" aria-label="Toggle Theme" className="" onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+            <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+            <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            <span className="sr-only">Toggle Theme</span>
+        </Button>
+    </div>
+
   )
 
 
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,16 +9,17 @@ const Navbar = () => {
 
     return(
         <div className="sm:flex  sm:justify-between py-3 px-4 border-b w-full">
-             <div className="flex px-4 border-r h-full ">
+             <div className="flex px-4  ">
                 <Image src="/logo3.png"width={40} height={80} alt="logo image" />            
-                <Link href="/" className="">
-                    <h1 className="text-xl font-sans font-bold mt-2 mx-2 leading-none" >HitPaw</h1>
+                <Link href="/" className="border-r h-full">
+                    <h1 className="text-xl font-sans font-bold mt-2 mx-2 leading-none pr-3" >HitPaw</h1>
                  </Link>
+                 <h1 className="text-lg font-sans mt-3 mx-2 leading-none text-gray-500 pl-3">AI Photo Enhancer</h1>
              </div>
+
              <ModeToggle ></ModeToggle>
         </div>
        
-        
     )
 }
 
